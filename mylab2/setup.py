ff03f0c0f096926282785749ac91f57d21182553
AICling <2460058215@qq.com>
2024-08-16 20:43:40 +0800

    add import support

--- mylab2/setup.py
+++ mylab2/setup.py
@@ -3,9 +3,18 @@ from torch.utils.cpp_extension import BuildExtension, CppExtension
 
 setup(
     name='custom_linear_cpp',
+    version='0.1',
+    author='Yuan Jiaqi',
+    author_email='2460058215@qq.com',
+    description='A custom C++ extension for PyTorch',
     ext_modules=[
-        CppExtension('custom_linear_cpp', ['custom_linear.cpp']),
+        CppExtension(
+            name='custom_linear_cpp',
+            sources=['custom_linear.cpp'],
+        ),
     ],
     cmdclass={
         'build_ext': BuildExtension
-    })
+    }
+)
+
