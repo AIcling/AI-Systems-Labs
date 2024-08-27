3e72d161eaa624a74cd6e7ceff6812bfa549f4b4
AICling <2460058215@qq.com>
2024-08-27 23:08:32 +0800

    cuda linear

--- mylab3/custom_cuda/custom.cu
+++ mylab3/custom_cuda/custom.cu
@@ -1,21 +1,48 @@
-__global__ void matrixMulKernel(float* input, float* weights, float* output, int N, int inputDim, int H) {
-    int row = blockIdx.x * blockDim.x + threadIdx.x; // 对应于输入矩阵的行
-    int col = blockIdx.y * blockDim.y + threadIdx.y; // 对应于输出矩阵的列
+#include <__clang_cuda_builtin_vars.h>
+#include <cuda_runtime.h>
+#include <torch/extension.h>
+#include <vector>
 
-    if (row < N && col < H) {
-        float value = 0.0;
-        for (int k = 0; k < inputDim; ++k) { 
-            value += input[row * inputDim + k] * weights[k * H + col];
+__global__ void linear_forward_kernel(const float* A, const float* B, const float* bias, float* C, int M, int N, int K){
+    int row = blockDim.x * blockIdx.x + threadIdx.x;
+    int col = blockDim.y * blockDim.y + threadIdx.y;
+
+    if(row < M && col < K){
+        float value = 0.0f;
+        for(int i=0;i<N;i++){
+            value += A[row * N + i] * B[K * i + col];
         }
-        output[row * H + col] = value;
+        C[row*K+col] = value + bias[col];
     }
 }
 
-void matrixMul(float* input, float* weights, float* output, int N, int inputDim, int H) {
-    dim3 blockDim(16, 16);
-    dim3 gridDim((N + blockDim.x - 1) / blockDim.x, (H + blockDim.y - 1) / blockDim.y);
+__global__ void linear_backward_kernel(
+    const float* grad_output, const float* input, const float* weights, 
+    float* grad_input, float* grad_weights, float* grad_bias, int M, int N, int K){
+    
+    int row = blockDim.x * blockIdx.x + threadIdx.x;
+    int col = blockDim.y * blockDim.y + threadIdx.y;
 
-    matrixMulKernel<<<gridDim, blockDim>>>(input, weights, output, N, inputDim, H);
-
-    cudaDeviceSynchronize();
-}
+    // cal grad_input (grad_output * weights^T)
+    if(row < M && col < K){
+        float value = 0.0f;
+        for(int i=0;i<N;i++){
+            value += grad_output[row*K +i] + weights[col*K +i];
+        }
+        grad_input[row*N+col] = value;
+    }
+    //cal grad_weights (input^T * grad_output)
+    if(row < N && col < K){
+        float value = 0.0f;
+        for(int i=0;i<M;i++){
+            value += input[i*N+row] + grad_output[i*K+col];
+        }
+        grad_weights[row*K+col] = value;
+    }
+    //cal grad_bias (the sum of grad_output's column)
+    if (threadIdx.x == 0 && threadIdx.y == 0) {
+        for (int i = 0; i < M; ++i) {
+            atomicAdd(&grad_bias[col], grad_output[i * K + col]);
+        }
+    }     
+} 
